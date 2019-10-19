f998ee6d88efa3965f41b7ca4214823a7645209f
trekimann <trekimann@gmail.com>
2019-10-19 23:43:56 +0100

    Started some module example testing

--- src/app/core/components/ApplicationWindow.tsx
+++ src/app/core/components/ApplicationWindow.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import { Example } from "../../modules/Example/components/example";
 
 export const ApplicationWindow = (props: any) => {
-  return <div className="applicationWindow">Application window</div>;
+  return <div className="applicationWindow">
+  <Example/>
+  </div>;
 };
---
+++ src/app/modules/Example/components/example.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "../style/exampleStyle.scss";
+
+export const Example = (props: any) => {
+  return (
+    <div className="applicationWindow Example">
+      EXAMPLE      
+    </div>
+  );
+};
