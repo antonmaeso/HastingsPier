d88b4701301d63f58cbd20ef18ef367687a58edd
trekimann <trekimann@gmail.com>
2019-10-20 17:09:05 +0100

    Fixed some app pane styling

--- src/app/modules/ApplicationSelection/components/ApplicationPane.tsx
+++ src/app/modules/ApplicationSelection/components/ApplicationPane.tsx
@@ -1,8 +1,14 @@
-import * as React from "react"
+import * as React from "react";
 
 export const AppPane = (props: any) => {
-    return <div className={"AppPane "+props.appName}>
-    {props.appName}
-    <div className="ShowOnMouseOver">{props.description}</div>
+  return (
+    <div className={"AppPane " + props.appName}>
+      {props.appName}
+      <div className="ShowOnMouseOver">{props.description}</div>
     </div>
+  );
+};
+
+const clickApp = (app: string) =>{
+    //signal to the util that an app has been clicked on
 }
