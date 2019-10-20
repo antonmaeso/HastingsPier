c7a4143853ed0b7adae0c07ec637c71ce35ba517
trekimann <trekimann@gmail.com>
2019-10-20 19:50:25 +0100

    Added App Tray and logo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { readJsonFile } from "./app/core/util/FileLoader";
 const url = require("url");
 const path = require("path");
+// const os = require("os");
+import logo from "./app/core/assets/Ant.png";
 
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow, ipcMain, Tray, nativeImage, Menu } from "electron";
 
 let window: BrowserWindow | null;
 
@@ -29,6 +31,13 @@ const createWindow = () => {
   window.on("closed", () => {
     window = null;
   });
+
+  createTray();
+
+  // BrowserWindow.addDevToolsExtension(
+  //   path.join(os.homedir(), 
+  //  '/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.2.0_0')
+  // )
 };
 
 app.on("ready", createWindow);
@@ -45,6 +54,48 @@ app.on("activate", () => {
   }
 });
 
+//app tray
+let appIcon: Tray = null;
+let lastBalloonMessage = "";
+const createTray = () => {
+  appIcon = new Tray(nativeImage.createFromPath( path.join(__dirname +"\\"+ logo)).resize({ width: 16, height: 16 }));
+
+  const contextMenu = Menu.buildFromTemplate([{
+    click() {
+      window.show();
+    },
+    label: "Show",
+  },
+  {
+    label: "Quit",
+    click() {
+      app.quit();
+    },
+  },
+  {
+    label: "Last message",
+    click() {
+      balloon("Last message", lastBalloonMessage);
+    },
+  },
+  ]);
+
+  appIcon.setContextMenu(contextMenu);
+  appIcon.setToolTip("Hastings Pier");
+  appIcon.on("double-click", () => { window.show(); });
+  appIcon.on("balloon-click", () => { window.show(); });
+};
+
+function balloon(displayTitle: string, contents: string) {
+  try {
+    appIcon.displayBalloon({ title: displayTitle, content: contents });
+    lastBalloonMessage = contents;
+  } catch (Exception) {
+    appIcon.displayBalloon({ title: "Exception", content: Exception });
+  }
+}
+
+
 // create static util classes
 const config = readJsonFile();
 ipcMain.on("apps", () => {
@@ -57,3 +108,9 @@ ipcMain.on("apps", () => {
 ipcMain.on("notify", (event: any, value: any) => {
   window.webContents.send("notify" + value.App, value);
 });
+
+//ipc routing for application pane
+ipcMain.on("AppBar", (event:any, value: any) => {
+  window.webContents.send("AppBar", value);
+})
+
