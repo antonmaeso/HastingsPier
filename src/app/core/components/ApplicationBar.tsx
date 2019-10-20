23718f60840200014bc74ce9da6f7b6a262fde4e
trekimann <trekimann@gmail.com>
2019-10-20 20:21:24 +0100

    Added Custom Scrollbar CSS

--- src/app/core/components/ApplicationBar.tsx
+++ src/app/core/components/ApplicationBar.tsx
@@ -1,32 +1,36 @@
 import * as React from "react";
 import { AppButton } from "./ApplicationButton";
 import { ipcRenderer } from "electron";
-import {Notify} from "../../core/util/Notify";
+import { Notify } from "../../core/util/Notify";
 
-const applicationHC = [ 
+const applicationHC = [
   // TODO: look this up from a util class
-  <AppButton title="ApplicationSelection"/>,
-  <AppButton title="Octane"/>,
-  <AppButton title="BatManager"/>
-]
+  <AppButton title="ApplicationSelection" />,
+  <AppButton title="Octane" />,
+  <AppButton title="BatManager" />
+];
 
 export const ApplicationBar = (props: any) => {
-  const[applications, setApplications] = React.useState([<AppButton title="ApplicationSelection"/>])
-  const listener = "AppBar"
+  const [applications, setApplications] = React.useState([
+    <AppButton title="ApplicationSelection" />
+  ]);
+  const listener = "AppBar";
   ipcRenderer.removeAllListeners(listener);
-  ipcRenderer.on(listener, (event: any, value: any)=>{
-    const newList = addApplicationToBar(value,applications)
-    setApplications(newList)   
-  })
-  Notify.Balloon("AppBar",applications.length+" Apps in state")
-  return <div className="applicationBar">
-  Applications
-  <br/>
-  {applications}
-  </div>;
+  ipcRenderer.on(listener, (event: any, value: any) => {
+    const newList = addApplicationToBar(value, applications);
+    setApplications(newList);
+  });
+  // Notify.Balloon("AppBar", applications.length + " Apps in state");
+  return (
+    <div className="applicationBar">
+      Applications
+      <br />
+      {applications}
+    </div>
+  );
 };
 
-const addApplicationToBar = (appName: string, existingApps: any[]) =>{
-  const newList = existingApps.concat([<AppButton title = {appName}/>])
-  return newList
-}
+const addApplicationToBar = (appName: string, existingApps: any[]) => {
+  const newList = existingApps.concat([<AppButton title={appName} />]);
+  return newList;
+};
