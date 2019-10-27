99765965294b80e5a1b5e258dd35cac92bb22d0c
anton_maeso <antonmaeso@hotmail.com>
2019-10-27 13:06:38 +0000

    Refactored DataHandler test for readability

--- src/tests/util/DataHandler/DataHandler.test.ts
+++ src/tests/util/DataHandler/DataHandler.test.ts
@@ -1,11 +1,11 @@
 import { DataHandler } from "../../../app/core/util/DataHandler/DataHandler";
 
 test("test read method called when file has already been created", () => {
-  const foo = new DataHandler();
-  const mockAddListener = jest.spyOn(foo, "create"); // spy on create
-  mockAddListener.mockImplementation(() => false); // mock result for create
-  const spyOnRead = jest.spyOn(foo, "read"); //spy on read to see if we call it
-  spyOnRead.mockImplementation(() => "");
-  foo.readOrCreateThenRead("", ""); // run method we are testing
+  const dataHandler = DataHandler.createDataHandler();
+  const mockCreate = jest.spyOn(dataHandler, "create"); // spy on create
+  mockCreate.mockImplementation(() => false); // mock result for create
+  const spyOnRead = jest.spyOn(dataHandler, "read"); //spy on read to see if we call it
+  spyOnRead.mockImplementation(() => ""); //default value
+  dataHandler.readOrCreateThenRead("", ""); // run method we are testing
   expect(spyOnRead).toHaveBeenCalled(); // success if spy called
 });
