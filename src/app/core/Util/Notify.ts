4e066208a0b7ed94b1976cb3f17501d1d0b11759
trekimann <trekimann@gmail.com>
2019-10-30 12:22:42 +0000

    Added JDocs

--- src/app/core/Util/Notify.ts
+++ src/app/core/Util/Notify.ts
@@ -1,25 +1,36 @@
 import { ipcRenderer } from "electron";
 
+/**
+ * Send a balloon notification with your title and contents
+ */
 export const Balloon = (Title: string, Contents: string, Other?: any) => {
     ipcRenderer.send("balloon", { title: Title, contents: Contents, other: Other });
 };
 
+/**
+ * Sends a notification to an application in the appBar
+ */
 export const AppNotification = (AppName: string, notificationToDisplay: string) => {
     ipcRenderer.send("AppBarNotify", { App: AppName, Notification: notificationToDisplay });
 };
 
+/**
+ * Changes the title displayed in the application
+ */
 export const setWindowTitle = (title: string, windowId?: number) => {
     ipcRenderer.send("menuTitle", { Title: title, WindowId: windowId });
 };
 
-// public static getWindowTitle = () =>{
-//     const toReturn = ipcRenderer.send("getMenuTitle");
-// }
-
+/**
+ * Sets the application which is currently being displayed
+ */
 export const setActiveApplication = (App: string, window?: number) => {
     ipcRenderer.send("activeApplication", { Active: App, WindowId: window });
 };
 
+/**
+ * Removes an application from the DOM.
+ */
 export const closeApplication = (App: string, windowId?: number) => {
     ipcRenderer.send("closeApplication", { Close: App, WindowId: windowId });
 };
--- src/app/core/Util/PersistantStorage.ts
+++ src/app/core/Util/PersistantStorage.ts
@@ -12,6 +12,10 @@ const storageAvailable = (type: string) => {
     }
 };
 
+/**
+ * Saves a value to local storage. Can be retrieved after an application has closed. If possible use session instead.
+ * Uses JSON.stringify to store
+ */
 export const putLocal = (key: string, value: any) => {
     try {
         if (storageAvailable("localStorage")) {
@@ -24,12 +28,20 @@ export const putLocal = (key: string, value: any) => {
     }
 };
 
+/**
+ * Retrieve a value from local storage
+ */
 export const getLocal = (key: string) => {
     const raw = localStorage.getItem(key);
     const toReturn = JSON.parse(raw);
     return toReturn;
 };
 
+/**
+ * Saves a value to Session storage. Can not be retrieved after an application has closed.
+ * If you need to retrieve after the application has close, use Local
+ * Uses JSON.stringify to store
+ */
 export const putSession = (key: string, value: any) => {
     try {
         if (storageAvailable("sessionStorage")) {
@@ -42,6 +54,9 @@ export const putSession = (key: string, value: any) => {
     }
 };
 
+/**
+ * Retrieve a value from session storage
+ */
 export const getSession = (key: string) => {
     const toReturn = JSON.parse(sessionStorage.getItem(key));
     return toReturn;
