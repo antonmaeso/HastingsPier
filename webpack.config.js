7c8166e32b33edffa4c2cdde5ff72f3d66031015
trekimann <trekimann@gmail.com>
2019-12-04 17:14:54 +0000

    Trying to get JEST to work async

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,49 @@
-
 const path = require("path");
 
 const config = {
-  target: "electron-main",
-  devtool: "source-map",
-  entry: "./src/main.ts",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(s*)css$/,
-        use: ['style-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+    target: "electron-main",
+    devtool: "source-map",
+    entry: ['babel-polyfill', "./src/main.ts"],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [{
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
-          },
-        ],
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
-  node: {
-    __dirname: false,
-    __filename: false
-  }
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    node: {
+        __dirname: false,
+        __filename: false
+    }
 };
 
 module.exports = (env, argv) => {
-  return config;
+    return config;
 };
