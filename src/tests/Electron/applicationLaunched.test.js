2a48221d40f627f78a5ec4a8d656ef48790dba32
Anton Maeso <maesoa@hastingsdirect.com>
2019-12-12 14:10:11 +0000

    Solved async test issue

--- src/tests/Electron/applicationLaunched.test.js
+++ src/tests/Electron/applicationLaunched.test.js
@@ -1,31 +1,46 @@
- test("Lanches app, checks its visible", async() => {
-     var Application = require('spectron').Application
-     var assert = require('assert')
-     const electronPath = require('electron')
-     const path = require('path')
+import "babel-polyfill";
+var Application = require("spectron").Application;
+var assert = require("assert");
+const electronPath = require("electron");
+const path = require("path");
 
-     var app = new Application({
-         path: electronPath,
-         args: [path.join(__dirname, "../../../dist/main.js")]
-     })
+test("Lanches app, checks its visible", async () => {
+  jest.setTimeout(30000);
+  var app = await new Application({
+    path: electronPath,
+    args: [path.join(__dirname, "../../../dist/main.js")]
+  });
 
-     await app.start().then(function() {
-         // Check if the window is visible
-         return app.browserWindow.isVisible()
-     }).then(function(isVisible) {
-         // Verify the window is visible
-         assert.equal(isVisible, true)
-     }).then(function() {
-         // Get the window's title
-         return app.client.getTitle()
-     }).then(function(title) {
-         // Verify the window's title
-         assert.equal(title, 'Hastings Pier')
-     }).then(function() {
-         // Stop the application
-         return app.stop()
-     }).catch(function(error) {
-         // Log any failures
-         console.error('Test failed', error.message)
-     })
- })
+  async function start() {
+    await app.start();
+    return app;
+  }
+
+  await start()
+    .then(function() {
+      // Check if the window is visible
+      return app.browserWindow.isVisible();
+    })
+    .then(function(isVisible) {
+      // Verify the window is visible
+      assert.equal(isVisible, true);
+    })
+    .then(function() {
+      // Get the window's title
+      return app.client.getTitle();
+    })
+    .then(function(title) {
+      // Verify the window's title
+      assert.equal(title, "Hastings Pier");
+    })
+    .then(function() {
+      // Stop the application
+      return app.stop();
+    })
+    .catch(function(error) {
+      // Log any failures
+      //console.error("Test failed", error.message);
+      app.stop();
+      throw error.message;
+    });
+});
